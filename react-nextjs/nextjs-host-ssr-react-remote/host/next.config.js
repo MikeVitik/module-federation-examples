17ac594c99abec7a309e7002ea264044540e8a97
MikeVitik <vitikm@gmail.com>
2024-04-18 12:37:26 +0300

    Update package version and change library type to commonjs2

--- react-nextjs/nextjs-host-ssr-react-remote/host/next.config.js
+++ react-nextjs/nextjs-host-ssr-react-remote/host/next.config.js
@@ -1,17 +1,20 @@
 const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+const { DefinePlugin } = require('webpack');
 
 module.exports = {
   webpack(config, options) {
-      config.plugins.push(
-          new NextFederationPlugin({
-            name: 'host',
-            remotes: {
-              remote: `remote@http://localhost:8081/${options.isServer ? 'server' : 'client'}/remoteEntry.js`,
-            },
-            filename: 'static/chunks/remoteEntry.js',
-          }),
-      );
-
+    config.plugins.push(
+      new DefinePlugin({ __IS_SERVER__: options.isServer }),
+      new NextFederationPlugin({
+        name: 'host',
+        remotes: {
+          remote: `remote@http://localhost:8081/${
+            options.isServer ? 'server' : 'client'
+          }/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+      }),
+    );
     return config;
   },
 };
